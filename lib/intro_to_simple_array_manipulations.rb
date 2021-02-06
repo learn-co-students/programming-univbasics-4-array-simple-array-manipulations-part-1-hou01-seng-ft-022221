09ae631f4e2404085bde2a161d0fe772ee7cc2b3
Jose Mendoza <pball626@gmail.com>
2021-02-06 21:27:39 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+
+def using_push(array, string)
+  using_push = ["green", "blue"]
+  using_push.push ("violet")
+end
+
+def using_unshift(array, string)
+  array.unshift ("Staten Island")
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
